87afe2e61c815a3e008c1700456e20df62ec77b1
Evgenyi Safronov <division494@gmail.com>
2025-03-09 12:18:32 +0300

    trigger CI

--- controlplane/internal/ffi/agent.go
+++ controlplane/internal/ffi/agent.go
@@ -35,7 +35,7 @@ type Agent struct {
 
 // TODO: docs for 'it's user responsibility to call "Close"'.
 func NewAgent(path string, name string, size uint) (*Agent, error) {
-	cPath := C.CString(path)
+	cPath := C.CString(pat)
 	defer C.free(unsafe.Pointer(cPath))
 
 	cName := C.CString(name)
