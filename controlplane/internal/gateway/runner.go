ef53018664297156bfecbce0d9d463fef5ecf653
Evgeny Safronov <division494@gmail.com>
2025-05-12 20:23:55 +0300

    refactor: unite built-in modules runner and register

--- controlplane/internal/gateway/runner.go
+++ controlplane/internal/gateway/runner.go
@@ -7,12 +7,14 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	"go.uber.org/zap"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
-	"github.com/yanet-platform/yanet2/controlplane/yncp/gateway"
+	"github.com/yanet-platform/yanet2/controlplane/ynpb"
 )
 
 type BuiltInModule interface {
@@ -77,13 +79,7 @@ func (m *BuiltInModuleRunner) Run(ctx context.Context) error {
 		return m.server.Serve(listener)
 	})
 
-	if err = gateway.RegisterModule(
-		ctx,
-		m.gatewayEndpoint,
-		listener,
-		m.module.ServicesNames(),
-		m.log,
-	); err != nil {
+	if err = m.register(ctx, listener.Addr()); err != nil {
 		return fmt.Errorf("failed to register services: %w", err)
 	}
 
@@ -115,4 +111,45 @@ func (m *BuiltInModuleRunner) listen() (net.Listener, error) {
 	}
 
 	return net.Listen("tcp", endpoint)
+}
+
+func (m *BuiltInModuleRunner) register(ctx context.Context, addr net.Addr) error {
+	gatewayConn, err := grpc.NewClient(
+		addr.String(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return fmt.Errorf("failed to initialize gateway gRPC client: %w", err)
+	}
+
+	client := ynpb.NewGatewayClient(gatewayConn)
+
+	wg, ctx := errgroup.WithContext(ctx)
+	for _, serviceName := range m.module.ServicesNames() {
+		serviceName := serviceName
+		req := &ynpb.RegisterRequest{
+			Name:     serviceName,
+			Endpoint: addr.String(),
+		}
+
+		wg.Go(func() error {
+			for {
+				if _, err := client.Register(ctx, req); err == nil {
+					m.log.Infof("successfully registered %q in the Gateway API", serviceName)
+					return nil
+				}
+				select {
+				case <-ctx.Done():
+					return ctx.Err()
+				default:
+				}
+
+				m.log.Warnf("failed to register %q in the Gateway API: %v", serviceName, err)
+				// TODO: exponential backoff should fit better here.
+				time.Sleep(1 * time.Second)
+			}
+		})
+	}
+
+	return wg.Wait()
 }
