a4924b24f29e194c03529a899ee0aba5c655c48f
Sergey Yakovlev <3gnees@gmail.com>
2025-06-23 18:28:13 +0300

    controlplane/agent.c: fixed loop condition on freeing unused agents.

--- lib/controlplane/agent/agent.c
+++ lib/controlplane/agent/agent.c
@@ -985,7 +985,7 @@ agent_free_unused_modules(struct agent *agent) {
 
 void
 agent_free_unused_agents(struct agent *agent) {
-	while (ADDR_OF(&agent->prev) != NULL) {
+	while ((agent != NULL) && (ADDR_OF(&agent->prev) != NULL)) {
 		struct agent *prev_agent = ADDR_OF(&agent->prev);
 
 		if (prev_agent->loaded_module_count == 0) {
