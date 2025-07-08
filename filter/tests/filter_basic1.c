7ecbbd5dd7170cc695e8a8687051dc4f1ee9862b
Sergey Yakovlev <3gnees@gmail.com>
2025-07-08 13:14:48 +0300

    fix tests

--- filter/tests/filter_basic1.c
+++ filter/tests/filter_basic1.c
@@ -23,7 +23,7 @@ main() {
 
 	// filter attributes
 	struct filter_attribute attributes[2] = {
-		port_src_attribute, attribute_port_dst
+		attribute_port_src, attribute_port_dst
 	};
 
 	// action 1:
