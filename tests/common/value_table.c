0a39feae8be504cc69a9ab578c9e4edb3c415ff3
Sergey Yakovlev <3gnees@gmail.com>
2025-07-10 13:39:36 +0300

    common/value_table test

---
+++ tests/common/value_table.c
@@ -0,0 +1,56 @@
+#include "common/value.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+int
+main() {
+	void *arena0 = malloc(1 << 24); // 16MB
+	if (arena0 == NULL) {
+		return 1;
+	}
+
+	struct block_allocator alloc;
+	block_allocator_init(&alloc);
+	block_allocator_put_arena(&alloc, arena0, 1 << 24);
+
+	struct memory_context mem_ctx;
+	if (memory_context_init(&mem_ctx, "test", &alloc) < 0) {
+		return 1;
+	}
+
+	struct value_table table;
+	value_table_init(&table, &mem_ctx, 1, 10);
+
+	uint32_t l[5] = {2, 3, 0, 8, 6};
+	uint32_t r[5] = {5, 7, 4, 9, 10};
+
+	uint32_t mask[10];
+	memset(mask, 0, sizeof(mask));
+
+	for (size_t i = 0; i < 5; ++i) {
+		value_table_new_gen(&table);
+		for (size_t x = l[i]; x < r[i]; ++x) {
+			mask[x] |= 1 << i;
+			value_table_touch(&table, 0, x);
+		}
+	}
+
+	value_table_compact(&table);
+
+	for (size_t i = 0; i < 5; ++i) {
+		for (size_t j = i + 1; j < 5; ++j) {
+			int res = (mask[i] == mask[j]) ^
+				  (value_table_get(&table, 0, i) ==
+				   value_table_get(&table, 0, j));
+			assert(res == 0);
+		}
+	}
+
+	value_table_free(&table);
+	free(arena0);
+
+	puts("OK");
+
+	return 0;
+}
