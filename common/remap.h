dae6b7e384ffe6c8ad335de4f2965e2169768ff4
Sergey Yakovlev <3gnees@gmail.com>
2025-07-10 18:14:08 +0300

    remap.h update

--- common/remap.h
+++ common/remap.h
@@ -25,7 +25,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define REMAP_TABLE_CHUNK_SIZE 65536
+#define REMAP_TABLE_CHUNK_SIZE 4096
 #define REMAP_TABLE_INVALID 0xffffffff
 
 /*
@@ -136,7 +136,7 @@ remap_table_item(struct remap_table *table, uint32_t key) {
  */
 static inline int
 remap_table_new_key(struct remap_table *table, uint32_t *key) {
-	if (false && table->free_list != REMAP_TABLE_INVALID) {
+/*	if (table->free_list != REMAP_TABLE_INVALID) {
 		*key = table->free_list;
 		struct remap_item *free_item = remap_table_item(table, *key);
 		table->free_list = free_item->value;
@@ -144,7 +144,7 @@ remap_table_new_key(struct remap_table *table, uint32_t *key) {
 		*free_item = (struct remap_item){0, 0, 0, 0};
 		return 0;
 	}
-
+*/
 	if (!(table->count % REMAP_TABLE_CHUNK_SIZE)) {
 		struct remap_item *new_chunk =
 			(struct remap_item *)memory_balloc(
@@ -163,10 +163,8 @@ remap_table_new_key(struct remap_table *table, uint32_t *key) {
 		struct remap_item **old_keys = ADDR_OF(&table->keys);
 
 		struct remap_item **new_keys =
-			(struct remap_item **)memory_brealloc(
+			(struct remap_item **)memory_balloc(
 				table->memory_context,
-				old_keys,
-				old_chunk_count * sizeof(struct remap_item *),
 				new_chunk_count * sizeof(struct remap_item *)
 			);
 		if (new_keys == NULL) {
@@ -179,8 +177,17 @@ remap_table_new_key(struct remap_table *table, uint32_t *key) {
 			return -1;
 		}
 
+		for (uint64_t chunk_idx = 0; chunk_idx < old_chunk_count; ++chunk_idx) {
+			SET_OFFSET_OF(
+				new_keys + chunk_idx,
+				ADDR_OF(old_keys + chunk_idx)
+			);
+		}
+
 		SET_OFFSET_OF(&new_keys[new_chunk_count - 1], new_chunk);
 		SET_OFFSET_OF(&table->keys, new_keys);
+
+		memory_bfree(table->memory_context, old_keys, old_chunk_count * sizeof(struct remap_item *));
 	}
 
 	struct remap_item *item = remap_table_item(table, table->count);
@@ -205,6 +212,8 @@ remap_table_touch(struct remap_table *table, uint32_t key, uint32_t *value) {
 	}
 
 	struct remap_item *new_item = remap_table_item(table, item->value);
+	new_item->value = item->value;
+	new_item->gen = table->gen;
 	// Update reference count
 	new_item->count++;
 	item->count--;
