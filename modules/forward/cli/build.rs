6f5ede41dee28d0b2c1fc196e81c77c7d2c437bf
Sergey Kacheev <s.kacheev@gmail.com>
2025-07-17 16:09:07 +0300

    refactor(forward): iterate through instances on the CLI side

--- modules/forward/cli/build.rs
+++ modules/forward/cli/build.rs
@@ -1,13 +1,17 @@
 use core::error::Error;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
+    println!("cargo:rerun-if-changed=../../../common/proto/target.proto");
     println!("cargo:rerun-if-changed=../controlplane/forwardpb/forward.proto");
 
     tonic_build::configure()
         .emit_rerun_if_changed(false)
         .build_server(false)
         .message_attribute(".", "#[derive(Serialize)]")
-        .compile_protos(&["forwardpb/forward.proto"], &["../controlplane"])?;
+        .compile_protos(
+            &["common/proto/target.proto", "forwardpb/forward.proto"],
+            &["../../..", "../controlplane"],
+        )?;
 
     Ok(())
 }
