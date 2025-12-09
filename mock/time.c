dfbc18f9293705515fa6270dc97364bb0145507c
Sergey Yakovlev <3gnees@gmail.com>
2025-12-09 21:33:21 +0300

    cosmetics

--- mock/time.c
+++ mock/time.c
@@ -1,21 +1,10 @@
 #include "time.h"
 
-<<<<<<< HEAD
 #include <stdint.h>
-=======
->>>>>>> 33f5f20 (feat: dataplane worker clock)
 #include <threads.h>
 
 thread_local struct timespec current_time = {0, 0};
 
-<<<<<<< HEAD
-=======
-void
-set_current_time(struct timespec *ts) {
-	current_time = *ts;
-}
-
->>>>>>> 33f5f20 (feat: dataplane worker clock)
 // Mock tsc clock
 
 struct tsc_clock;
@@ -32,16 +21,9 @@ tsc_clock_adjust(struct tsc_clock *clock) {
 	return 0;
 }
 
-<<<<<<< HEAD
 uint64_t
 tsc_clock_get_time_ns(struct tsc_clock *clock) {
 	(void)clock;
 	return current_time.tv_sec * (uint64_t)1000 * 1000 * 1000 +
 	       current_time.tv_nsec;
-=======
-struct timespec
-tsc_clock_get_time(struct tsc_clock *clock) {
-	(void)clock;
-	return current_time;
->>>>>>> 33f5f20 (feat: dataplane worker clock)
 }
