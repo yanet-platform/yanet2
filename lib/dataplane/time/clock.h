46270421f8504aac00b80f54d585d52d9fc4f2fe
Sergey Yakovlev <3gnees@gmail.com>
2025-12-11 18:33:07 +0300

    rebase main

--- lib/dataplane/time/clock.h
+++ lib/dataplane/time/clock.h
@@ -1,10 +1,6 @@
 #pragma once
 
 #include <stdint.h>
-<<<<<<< HEAD
-=======
-#include <time.h>
->>>>>>> 33f5f20 (feat: dataplane worker clock)
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -24,13 +20,8 @@
 // clock drift on TSC with 1ppm drift
 // (modern CPUs have drift of 0.1-1 ppm).
 struct tsc_clock {
-<<<<<<< HEAD
 	// Real time when clock was init in nanoseconds.
 	uint64_t real_time_ns;
-=======
-	// Real time when clock was init.
-	struct timespec real_time;
->>>>>>> 33f5f20 (feat: dataplane worker clock)
 
 	// Timestamp counter when clock was init.
 	uint64_t timestamp_counter;
@@ -44,12 +35,6 @@ tsc_clock_init(struct tsc_clock *clock);
 int
 tsc_clock_adjust(struct tsc_clock *clock);
 
-<<<<<<< HEAD
 // Get current real time in nanoseconds.
 uint64_t
 tsc_clock_get_time_ns(struct tsc_clock *clock);
-=======
-// Get current real time.
-struct timespec
-tsc_clock_get_time(struct tsc_clock *clock);
->>>>>>> 33f5f20 (feat: dataplane worker clock)
