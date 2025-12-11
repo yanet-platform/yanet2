58107a0ed1a234c77289be5b6caa81d5e45bf4c9
Sergey Yakovlev <3gnees@gmail.com>
2025-12-11 18:33:07 +0300

    rebase main

--- mock/tests/my_module/config.h
+++ mock/tests/my_module/config.h
@@ -9,9 +9,5 @@
 struct my_module_config {
 	struct cp_module cp_module;
 	size_t packet_counter;
-<<<<<<< HEAD
 	uint64_t last_packet_timestamp;
-=======
-	struct timespec last_packet_timestamp;
->>>>>>> 33f5f20 (feat: dataplane worker clock)
 };
