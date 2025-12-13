285eee8386b719821cd160d04f961967dbfdd8c3
Sergey Yakovlev <3gnees@gmail.com>
2025-12-13 19:11:27 +0300

    cf

--- tests/common/ttlmap_test.c
+++ tests/common/ttlmap_test.c
@@ -1,6 +1,6 @@
+#include "../../lib/logging/log.h"
 #include "common/memory_block.h"
 #include "rte_common.h"
-#include "../../lib/logging/log.h"
 
 #include <common/ttlmap/detail/bucket.h>
 #include <common/ttlmap/detail/lock.h>
