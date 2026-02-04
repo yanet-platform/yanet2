2abd529b0d15ce210a93bc030eb6f63506e43e98
Evgeny Safronov <division494@gmail.com>
2026-02-04 09:49:57 +0300

    feat: build info for yanet-dataplane
    yanet-dataplane 0.0.1
      Compiler:   gcc 13.3.0
      Build type: release
      Built:      2026-02-04 09:46:02
      Git commit: 0064682a
      DPDK:       DPDK 24.03.0

--- dataplane/main.c
+++ dataplane/main.c
@@ -1,23 +1,71 @@
+#include <getopt.h>
 #include <stdio.h>
+
+#include <rte_version.h>
 
 #include "config.h"
 #include "dataplane.h"
 #include "logging/log.h"
+#include "yanet_build_config.h"
+
+static void
+print_version(void) {
+	printf("yanet-dataplane %s\n", YANET_VERSION);
+	printf("  Compiler:   %s %s\n",
+	       YANET_COMPILER_ID,
+	       YANET_COMPILER_VERSION);
+	printf("  Build type: %s\n", YANET_BUILD_TYPE);
+	printf("  Built:      %s\n", YANET_BUILD_DATE);
+	printf("  Git commit: %s\n", YANET_GIT_COMMIT);
+	printf("  DPDK:       %s\n", rte_version());
+}
+
+static void
+print_usage(const char *prog) {
+	printf("Usage: %s [OPTIONS] CONFIG_PATH\n", prog);
+	printf("Options:\n");
+	printf("  -v, --version   Print version information and exit\n");
+	printf("  -h, --help      Print this help message and exit\n");
+}
 
 int
 main(int argc, char **argv) {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s CONFIG_PATH\n", argv[0]);
+	static struct option long_options[] = {
+		{"version", no_argument, 0, 'v'},
+		{"help", no_argument, 0, 'h'},
+		{0, 0, 0, 0}
+	};
+
+	int opt;
+	while ((opt = getopt_long(argc, argv, "vh", long_options, NULL)) != -1
+	) {
+		switch (opt) {
+		case 'v':
+			print_version();
+			return 0;
+		case 'h':
+			print_usage(argv[0]);
+			return 0;
+		default:
+			print_usage(argv[0]);
+			return -1;
+		}
+	}
+
+	if (optind >= argc) {
+		fprintf(stderr, "Error: CONFIG_PATH is required\n");
+		print_usage(argv[0]);
 		return -1;
 	}
+
 	// This function initializes and enables logging
 	log_enable_name("debug");
 
 	struct dataplane_config *config;
-	FILE *config_file = fopen(argv[1], "r");
+	FILE *config_file = fopen(argv[optind], "r");
 	LOG(INFO, "initialize the dataplane config");
 	if (dataplane_config_init(config_file, &config)) {
-		LOG(ERROR, "invalid config file: %s", argv[1]);
+		LOG(ERROR, "invalid config file: %s", argv[optind]);
 		return -1;
 	}
 
