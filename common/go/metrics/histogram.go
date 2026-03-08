ae4c9e8aa5e5dbf244bed645c99b27d0c40ba7ef
GeorgyKirichenko <kirichenkoga@gmail.com>
2026-03-08 18:54:52 +0300

    Revert "metrics refactoring + metric map implementation + test stubs"
    This reverts commit 289005d26b883c72f4a7ab294313c7c4480aa637.

--- common/go/metrics/histogram.go
+++ common/go/metrics/histogram.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Histogram struct {
-	Bounds []float64
+	bounds []float64
 
-	// Buckets holds the counters.
-	// len(Buckets) == len(bounds) + 1.
+	// buckets holds the counters.
+	// len(buckets) == len(bounds) + 1.
 	// The last bucket is for values > the last bound (+Inf).
-	Buckets []atomic.Uint64
+	buckets []atomic.Uint64
 }
 
 func NewHistogram(bounds []float64) *Histogram {
@@ -20,16 +20,20 @@ func NewHistogram(bounds []float64) *Histogram {
 	sort.Float64s(sorted)
 
 	return &Histogram{
-		Bounds: sorted,
+		bounds: sorted,
 		// we need 1 extra bucket for the "infinite" bucket (values > max bound)
-		Buckets: make([]atomic.Uint64, len(sorted)+1),
+		buckets: make([]atomic.Uint64, len(sorted)+1),
 	}
 }
 
 // Observe records a new value.
 // Complexity: O(log N) for search + O(1) for atomic write.
 func (h *Histogram) Observe(value float64) {
-	idx := sort.SearchFloat64s(h.Bounds, value)
+	idx := sort.SearchFloat64s(h.bounds, value)
 
-	h.Buckets[idx].Add(1)
+	h.buckets[idx].Add(1)
+}
+
+func (h *Histogram) Buckets() []atomic.Uint64 {
+	return h.buckets
 }
--- common/go/metrics/metric.go
+++ common/go/metrics/metric.go
@@ -1,7 +1,5 @@
 package metrics
 
-import "slices"
-
 type Label struct {
 	Name  string
 	Value string
@@ -11,17 +9,3 @@ type MetricID struct {
 	Name   string
 	Labels []Label
 }
-
-func (a MetricID) EqualOrdered(b MetricID) bool {
-	return a.Name == b.Name && slices.Equal(a.Labels, b.Labels)
-}
-
-type IsMetricValue interface {
-	isMetricValue()
-}
-
-func (c *Counter) isMetricValue() {}
-
-func (g *Gauge) isMetricValue() {}
-
-func (h *Histogram) isMetricValue() {}
