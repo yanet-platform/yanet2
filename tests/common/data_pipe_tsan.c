d2ce828cd8edf81f67142daf11967a47f41b518b
Evgeny Safronov <division494@gmail.com>
2026-03-29 20:21:46 +0300

    fix: align data_pipe_test with actual pattern from worker.c

--- tests/common/data_pipe_tsan.c
+++ tests/common/data_pipe_tsan.c
@@ -9,75 +9,72 @@
 #define NUM_ITERATIONS 100000
 
 struct data_pipe shared_pipe;
-volatile int producer_done = 0;
 
 static size_t
 push_handler(void **items, size_t count, void *ctx) {
 	uintptr_t *val = (uintptr_t *)ctx;
-	if (count == 0) {
-		return 0;
+	if (count > 0) {
+		items[0] = (void *)(*val);
+		return 1;
 	}
-	items[0] = (void *)(*val);
-	return 1;
+	return 0;
 }
 
 static size_t
 pop_handler(void **items, size_t count, void *ctx) {
-	uintptr_t *out = (uintptr_t *)ctx;
-	if (count == 0) {
-		return 0;
+	uintptr_t *expected = (uintptr_t *)ctx;
+	if (count > 0) {
+		uintptr_t val = (uintptr_t)items[0];
+		if (val != *expected) {
+			fprintf(stderr,
+				"DATA CORRUPTION: expected %lu, got %lu\n",
+				(unsigned long)*expected,
+				(unsigned long)val);
+			return 0; // Don't process corrupted data.
+		}
+		*expected += 1;
+		return 1;
 	}
-	*out = (uintptr_t)items[0];
-	return 1;
+	return 0;
 }
 
 static size_t
 free_handler(void **items, size_t count, void *ctx) {
 	(void)items;
 	(void)ctx;
-	if (count == 0) {
-		return 0;
+	if (count > 0) {
+		return 1;
 	}
-	return 1;
+	return 0;
 }
 
 static void *
 producer_thread(void *arg) {
 	(void)arg;
+
 	for (uintptr_t i = 1; i <= NUM_ITERATIONS; i++) {
 		uintptr_t val = i;
 		while (data_pipe_item_push(&shared_pipe, push_handler, &val) ==
 		       0) {
+			// If push failed, try to free some items to make space.
+			data_pipe_item_free(&shared_pipe, free_handler, NULL);
 		}
 	}
-	producer_done = 1;
+
+	printf("Producer pushed %lu items\n", (unsigned long)NUM_ITERATIONS);
 	return NULL;
 }
 
 static void *
 consumer_thread(void *arg) {
 	(void)arg;
-	uintptr_t received = 0;
 	uintptr_t expected = 1;
 
-	while (received < NUM_ITERATIONS) {
-		uintptr_t val = 0;
-		if (data_pipe_item_pop(&shared_pipe, pop_handler, &val) == 0) {
-			continue;
-		}
-
-		if (val != expected) {
-			fprintf(stderr,
-				"DATA CORRUPTION: expected %lu, got %lu\n",
-				(unsigned long)expected,
-				(unsigned long)val);
-		}
-		expected++;
-		received++;
-
-		data_pipe_item_free(&shared_pipe, free_handler, NULL);
+	while (expected <= NUM_ITERATIONS) {
+		data_pipe_item_pop(&shared_pipe, pop_handler, &expected);
 	}
 
+	printf("Consumer processed %lu items\n", (unsigned long)expected - 1);
 	return NULL;
 }
 
