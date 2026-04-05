e8977648ca294d6168ef178ac63e4fa111a88772
Evgeny Safronov <division494@gmail.com>
2026-04-05 14:00:32 +0300

    refactor(netip): move netip to its separate repository
    Simplify integration for the following things:
    - Separated modules (not in the main repo) may use it without cloning
      yanet2 repo.
    - Pushed to crates.io.
    - Internal usage, no code duplication.

--- common/rust/netip/src/lib.rs
+++
@@ -1,8 +0,0 @@
-mod macaddr;
-mod net;
-
-pub use macaddr::{MacAddr, MacAddrParseError};
-pub use net::{
-    Contiguous, ContiguousIpNetParseError, IpNetParseError, IpNetwork, Ipv4Network, Ipv4NetworkAddrs, Ipv6Network,
-    Ipv6NetworkAddrs, ipv4_binary_split, ipv6_binary_split,
-};
