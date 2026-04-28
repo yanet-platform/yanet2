70b31059b15547607f4aa5e89e4c8a338cac6d28
Evgeny Safronov <division494@gmail.com>
2026-04-28 09:22:12 +0300

    refactor(cli): no unsafe

--- cli/core/src/dispatcher.rs
+++ cli/core/src/dispatcher.rs
@@ -5,6 +5,7 @@ use std::{
     fs,
     io::ErrorKind,
     os::unix::{fs::PermissionsExt, process::CommandExt},
+    path::PathBuf,
     process::{self, Stdio},
 };
 
@@ -30,31 +31,25 @@ pub trait Dispatch {
     fn on_sub_binary_not_found(&self, subcommand: &str, modules: &HashSet<String>);
 }
 
-/// Initializes the environment by setting the `PATH` environment variable.
-///
-/// This is necessary to correctly locate the submodule executable using a
-/// relative path.
-pub fn init_path() {
-    let parent_path = match env::current_exe() {
-        Ok(exe) => exe.parent().expect("must have parent path").to_path_buf(),
-        Err(err) => {
-            eprintln!("error: {}", err);
-            process::exit(1);
-        }
-    };
+fn search_paths() -> Vec<PathBuf> {
+    let mut paths = env::split_paths(&env::var_os("PATH").unwrap_or_default()).collect::<Vec<_>>();
 
-    let path = env::var("PATH").unwrap_or_default();
-    unsafe {
-        // SAFETY: called from a single-thread application.
-        env::set_var("PATH", format!("{}:{}", path, parent_path.display()));
+    let parent = env::current_exe()
+        .ok()
+        .and_then(|exe| exe.parent().map(|v| v.to_path_buf()));
+
+    if let Some(parent) = parent {
+        paths.push(parent);
     }
+
+    paths
 }
 
 /// Locates executable binaries with prefix `prefix` in the `PATH` environment
 /// variable.
 pub fn locate_modules(prefix: &str) -> Result<HashSet<String>, Box<dyn Error>> {
     let mut modules = HashSet::new();
-    for path in env::split_paths(&env::var_os("PATH").unwrap_or_default()) {
+    for path in search_paths() {
         if !path.is_dir() {
             continue;
         }
@@ -104,6 +99,19 @@ pub fn locate_modules(prefix: &str) -> Result<HashSet<String>, Box<dyn Error>> {
     }
 
     Ok(modules)
+}
+
+fn locate_sub_binary(prefix: &str, subcommand: &str) -> Option<PathBuf> {
+    let subcommand = format!("{prefix}{subcommand}");
+
+    for path in search_paths() {
+        let path = path.join(&subcommand);
+        if path.is_file() {
+            return Some(path);
+        }
+    }
+
+    None
 }
 
 pub fn add_subcommands(mut command: Command, modules: &HashSet<String>) -> Command {
@@ -169,17 +177,18 @@ pub fn try_complete(name: &str, prefix: &str, behavior: &impl Dispatch) {
                     return;
                 }
             };
-            unsafe {
-                // SAFETY: called from a single-thread application.
-                env::set_var("_CLAP_COMPLETE_INDEX", format!("{}", idx - 1));
-            }
 
-            let subcommand = format!("{prefix}{cmd}");
+            let Some(subcommand) = locate_sub_binary(prefix, cmd) else {
+                return;
+            };
+            let subcommand_name = format!("{prefix}{cmd}");
+
             _ = process::Command::new(&subcommand)
                 .arg("--")
-                .arg(subcommand)
+                .arg(subcommand_name)
                 .args(args)
                 .stderr(Stdio::null())
+                .env("_CLAP_COMPLETE_INDEX", format!("{}", idx - 1))
                 .exec();
             return;
         }
@@ -189,7 +198,6 @@ pub fn try_complete(name: &str, prefix: &str, behavior: &impl Dispatch) {
 }
 
 pub fn dispatch(name: &str, prefix: &str, behavior: &impl Dispatch) -> ! {
-    init_path();
     try_complete(name, prefix, behavior);
 
     let modules = locate_modules(prefix).unwrap_or_default();
@@ -210,14 +218,19 @@ pub fn dispatch(name: &str, prefix: &str, behavior: &impl Dispatch) -> ! {
     let args = collect_args(matches);
 
     let subcommand = format!("{prefix}{cmd}");
-    let err = process::Command::new(&subcommand).args(args).exec();
+    let Some(path) = locate_sub_binary(prefix, cmd) else {
+        behavior.on_sub_binary_not_found(&subcommand, &modules);
+        process::exit(1);
+    };
+
+    let err = process::Command::new(&path).args(args).exec();
 
     match err.kind() {
         ErrorKind::NotFound => {
             behavior.on_sub_binary_not_found(&subcommand, &modules);
         }
         err => {
-            eprintln!("error: {subcommand} - {err}");
+            eprintln!("error: {} - {err}", path.display());
         }
     }
 
