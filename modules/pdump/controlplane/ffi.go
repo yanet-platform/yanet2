d63e31a286f073cbb2c4e2fac6e1161e2dbee143
Sergey Kacheev <s.kacheev@gmail.com>
2026-05-05 12:58:35 +0300

    build(pdump): vendor libpcap via cmake.subproject()
    Drop the run_command-based external CMake build and let Meson import
    libpcap as a native subproject. Bump submodule to libpcap-1.10.6 (fixes
    the "Cycle in CMake inputs/dependencies" error from 1.10.5), silence
    third-party noise (werror/warning_level off, -U_GNU_SOURCE,
    CMAKE_MESSAGE_LOG_LEVEL=WARNING, CMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON),
    and link ffi.go directly against the resulting libpcap_static.a so
    standalone `go build` keeps working.

--- modules/pdump/controlplane/ffi.go
+++ modules/pdump/controlplane/ffi.go
@@ -1,9 +1,8 @@
 package pdump
 
-//#cgo CFLAGS: -I../../../ -I../dataplane
-//#cgo CFLAGS: -I../../../build/modules/pdump/api/external/libpcap-install/include
+//#cgo CFLAGS: -I../../../ -I../dataplane -I../../../subprojects/libpcap
 //#cgo LDFLAGS: -L../../../build/modules/pdump/api -lpdump_cp
-//#cgo LDFLAGS: -L../../../build/modules/pdump/api/external/libpcap-install/lib -Wl,-Bstatic -lpcap -Wl,-Bdynamic
+//#cgo LDFLAGS: -L../../../build/subprojects/libpcap -l:libpcap_static.a
 //
 //#include <stdlib.h>
 //#include "modules/pdump/api/controlplane.h"
