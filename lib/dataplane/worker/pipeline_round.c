ab9191224a970a2a5dcde2140425a3a6f6a6ad37
Sergey Yakovlev <3gnees@gmail.com>
2026-06-05 16:58:54 +0300

    move inside

--- lib/dataplane/worker/pipeline_round.c
+++ lib/dataplane/worker/pipeline_round.c
@@ -14,25 +14,27 @@ worker_pipeline_round(
 	struct config_gen_ectx *config_gen_ectx,
 	struct packet_front *packet_front
 ) {
-	uint64_t device_count =
-		cp_config_gen->device_registry.registry.capacity;
-
-	struct device_ectx *devices[device_count];
-	struct packet_front *input[device_count];
-	struct packet_front *output[device_count];
-	for (uint64_t idx = 0; idx < device_count; ++idx) {
-		struct device_ectx *device =
-			ADDR_OF(config_gen_ectx->devices + idx);
-		input[idx] = device != NULL ? ADDR_OF(&device->pending_input) +
-						      dp_worker->idx
-					    : NULL;
-		output[idx] = device != NULL ? ADDR_OF(&device->pending_output
-					       ) + dp_worker->idx
-					     : NULL;
-		devices[idx] = device;
-	}
-
 	while (1) {
+		uint64_t device_count =
+			cp_config_gen->device_registry.registry.capacity;
+
+		struct device_ectx *devices[device_count];
+		struct packet_front *input[device_count];
+		struct packet_front *output[device_count];
+		for (uint64_t idx = 0; idx < device_count; ++idx) {
+			struct device_ectx *device =
+				ADDR_OF(config_gen_ectx->devices + idx);
+			input[idx] = device != NULL
+					     ? ADDR_OF(&device->pending_input) +
+						       dp_worker->idx
+					     : NULL;
+			output[idx] = device != NULL
+					      ? ADDR_OF(&device->pending_output
+						) + dp_worker->idx
+					      : NULL;
+			devices[idx] = device;
+		}
+
 		struct packet *packet;
 
 		int empty = 1;
