3cd5bf4ab4aa509efe8dc2825e8391a2cd9347aa
Konstantin Sazonov <moonug@yandex-team.ru>
2026-08-28 17:45:09 +0300

    fix(lab): restore capture timeout classification

--- tests/functional/framework/framework.go
+++ tests/functional/framework/framework.go
@@ -3,6 +3,7 @@ package framework
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -17,6 +18,10 @@ import (
 	"github.com/gopacket/gopacket/layers"
 	"go.uber.org/zap"
 )
+
+// ErrCaptureTimeout indicates that packet transmission succeeded but no packet
+// was captured before the receive deadline.
+var ErrCaptureTimeout = errors.New("packet capture timed out")
 
 const (
 	// MAC addresses used in test framework
