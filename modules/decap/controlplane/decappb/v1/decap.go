c224620b8427704c811b4d1e273ae0ac695bf081
Evgeny Safronov <division494@gmail.com>
2026-09-15 18:09:21 +0300

    refactor(decap): validate grpc requests in pb messages
    - Move the config name rule of ShowConfig, UpdateConfig and DeleteConfig into `Validate()` on the request messages, enforced by the validate interceptor from #2642.
    - Change the rejection text from `config name is required` to `name is required`, still InvalidArgument.
    - Keep prefix conversion in the UpdateConfig handler.
    - Move the empty-name cases out of the handler tests into table tests beside the pb file.

---
+++ modules/decap/controlplane/decappb/v1/decap.go
@@ -0,0 +1,27 @@
+package decappb
+
+import "errors"
+
+func (m *ShowConfigRequest) Validate() error {
+	if m.GetName() == "" {
+		return errors.New("name is required")
+	}
+
+	return nil
+}
+
+func (m *UpdateConfigRequest) Validate() error {
+	if m.GetName() == "" {
+		return errors.New("name is required")
+	}
+
+	return nil
+}
+
+func (m *DeleteConfigRequest) Validate() error {
+	if m.GetName() == "" {
+		return errors.New("name is required")
+	}
+
+	return nil
+}
